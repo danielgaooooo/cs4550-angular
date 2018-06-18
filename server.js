c596d5ce614190463d49d3c5fc14d61400a15a7f
Daniel Gao <danielgaooooo@gmail.com>
2018-06-18 17:19:49 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/cs4550-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname +'/dist/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/cs4550-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 8080);
