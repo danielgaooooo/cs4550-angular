7b1e40cfd8bbea464b097feb269e99ffb3645126
Daniel Gao <danielgaooooo@gmail.com>
2018-06-18 20:03:39 -0400

    hello

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('./dist/cs4550-angular'));
 
 
 app.get('/*all', function(req,res) {
-  res.sendFile(path.join(__dirname, './dist/cs4550-angular/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/cs4550-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
