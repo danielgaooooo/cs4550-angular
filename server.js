b7df53d5c0291bdef547d2c9f980317717403919
Daniel Gao <danielgaooooo@gmail.com>
2018-06-18 20:24:05 -0400

    hello

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/cs4550-angular'));
 
+app.get('/*', function(req,res) {
 
-app.get('/*all', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/cs4550-angular/index.html'));
+  res.sendFile(path.join('./dist/cs4550-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8000);
-
+app.listen(process.env.PORT || 8080);
